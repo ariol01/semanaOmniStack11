5eec917b96cb15950ee7f23e3e924e2dac5a8b23
Ariel Veras <ariolvs123@gmail.com>
2020-03-25 07:16:04 -0400

    [tarefa] dependencia nodemon
    [new] foi adicionado o pct nodemon para pegar atulização do site em tempo real

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,24 @@ const express = require('express');
 // armazena minha aplicacao 
 const app = express();
 
+app.use(express.json());
+
+//existem requisoções http
+// GET, POST, PUT, DELETE
+
+//Tipos de parametros
+//query params = dados nomeados enviado por uma url
+//route params = serve para acessar algum recurso
+// request body = corpo da requisição alterar ou criar recursos 
+
 app.get('/', (request, response)=>{
 
-response.send("Ariel deu hello world /n");
-response.send().json({
+const body = request.body;
+console.log(body);
+
+response.json({
   evento:'semana omnistack 11', 
-  aluno: 'ariel V.'
+  aluno: 'ariel Veras'
 });
 
 });
