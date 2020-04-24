048bb3ac608a34c4bbccfca7159808aec78823e3
Ariel Veras <ariolvs123@gmail.com>
2020-04-23 21:35:23 -0400

    [tarefa] Controllers
    [ref] agora tem uma pasta para armazenas as rotas e so chama-las em route.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,37 +1,8 @@
 const express = require('express');
 const routes = express.Router();
-const crypto = require('crypto');
-
-const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
 
 routes.use(express.json());
-
-routes.get('/ongs', async (request , response) =>{
-
-  const ongs = await connection('ongs').select('*');
-  response.json(ongs);
-});
-
-routes.post('/ongs', async (request, response) => {
-
-  const { name, email, whatsapp, city, uf } = request.body;
-  const data = request.body;
-
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-  });
-
-  console.log(data);
-
-  response.json({ id });
-
-});
-
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 module.exports = routes;
