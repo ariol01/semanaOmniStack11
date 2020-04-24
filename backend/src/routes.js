e555fd2080528e5098d4f13a8e74ea26801b625e
Ariel Veras <ariolvs123@gmail.com>
2020-04-23 21:25:00 -0400

    [tarefa] conexao com banco
    [new] foi criada conexão com banco de dados , feito rotas assicronas  e também retornar valor do banco de dados via get

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const routes = express.Router();
+const crypto = require('crypto');
 
+const connection = require('./database/connection');
 
 routes.use(express.json());
 
-routes.get('/', (request, response)=>{
+routes.get('/ongs', async (request , response) =>{
 
-const body = request.body;
-console.log(body);
-  response.json({
-    evento:'semana omnistack 11', 
-    aluno: 'ariel Veras'
+  const ongs = await connection('ongs').select('*');
+  response.json(ongs);
+});
+
+routes.post('/ongs', async (request, response) => {
+
+  const { name, email, whatsapp, city, uf } = request.body;
+  const data = request.body;
+
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf,
   });
-  
-  });
-  
+
+  console.log(data);
+
+  response.json({ id });
+
+});
+
 module.exports = routes;
